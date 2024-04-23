a2ba5e0b44164be05c548e408260b9159824dd1a
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-22 23:05:32 -0700

    Created a simple structure with some parallax elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import sunset from './assets/sunset-nightsky.jpg'
+import sunset2 from './assets/sunset-nightsky2F.png'
+import sunset3 from './assets/sunset-nightsky3F.png'
+import mt1 from './assets/lens1.jpg'
+import mt2 from './assets/layer1F.png'
+import mt3 from './assets/layer2F.png'
+import mt4 from './assets/layer3F.png'
+import Navbar from './components/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
+      <Navbar/>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Parallax pages={4} className="d-flex justify-content-center">
+          <ParallaxLayer className="d-flex justify-content-center"
+            offset={0}
+            speed={0.5}
+            factor={3}
+            style={{
+              backgroundImage: `url(${sunset})`,
+              backgroundSize: `cover`
+              
+            }}
+            >
+            <h2 className='mt-5'>About Me</h2>
+          </ParallaxLayer>
+
+          
+          <ParallaxLayer className="d-flex justify-content-center" 
+            offset={0.5}
+            speed={0.5}
+            >
+            <h3>This is my website</h3>
+          </ParallaxLayer>
+
+
+
+        </Parallax>
+
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <header>
+        <div className="container">
+            <div className="row">
+                <ul className="d-flex flex-row justify-content-center" id="navbar-text">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+  )
+}
